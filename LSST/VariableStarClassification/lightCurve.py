e1cebe2ec49a7ccb31ff40e7a209a36c5eff9253
leightontanner <tanner.leighton@gmail.com>
2017-06-28 11:28:22 -0500

    Update lightCurve.py
    Updated the lightCurve plotting function to be more general to work with a more general data set; in particular, the previous version assumes the oid changes at particular locations in the list, and the software of this new version determines that. This version still assumes there are at least two unique object id's and no more than three.

--- LSST/VariableStarClassification/lightCurve.py
+++ LSST/VariableStarClassification/lightCurve.py
@@ -6,13 +6,180 @@
     Tanner Leighton
 
     Written for CIERA Summer Internship, Northwestern University.
-    6/25/17
+    6/28/17
 """
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
-
+  
 def plotLightCurve(filename):
+    """ This function works with time, mag, error and DOES filter by oid.
+        Takes in four lists of data for each of the above mentioned things
+        and then makes three plots (of the three objects), which is what
+        is meant by filtering by oid
+
+        Arguments:
+            filename (string) : name of data txt file       
+    """
+
+    xList = []
+    yList = []
+    errorList = []
+    oidList = []
+    fidList = []
+
+    fin = open(filename)
+    for line in fin:
+        #line = line.strip()
+        line = line.split() # Splits each line into constituent numbers. 
+        xList.append(line[0])
+        yList.append(line[1])
+        errorList.append(line[2])
+        oidList.append(line[3])
+        fidList.append(line[4])
+    fin.close()
+
+    xListPrime = []
+    for element in xList:
+        xListPrime.append(float(element))
+    xList = xListPrime # so not using 'xListPrime' going forward
+
+    yListPrime = []
+    for element in yList:
+        yListPrime.append(float(element))
+    yList = yListPrime
+
+    errorListPrime = []
+    for element in errorList:
+        errorListPrime.append(float(element))
+    errorList = errorListPrime
+
+    oidListPrime = []
+    for element in oidList:
+        oidListPrime.append(int(element))
+    oidList = oidListPrime
+
+    fidListPrime = []
+    for element in fidList:
+        fidListPrime.append(int(element))
+    fidList = fidListPrime
+
+    """
+    This plots all objects ID's on same plot:
+    plt.errorbar(xList, yList, yerr = errorList, fmt='ro', markersize=3) 
+    plt.title("Star 2 Light Curve")
+    plt.xlabel("time")
+    plt.ylabel("brightness")
+    plt.show()
+    """
+
+    fmtToBeUsed = 'ro'
+    xListTemp = []
+    yListTemp = []
+    errorListTemp = []
+
+    # first make the code for three oid's
+    # then generalize code so it works for any number
+    # (generalized code better but don't need to right now,
+    # not for what I am trying to do here)
+
+    index = 0
+
+    tempOID = oidList[index]
+
+    oidListLength = len(oidList)
+    listLength = oidListLength # better to use this generic name everywhere
+
+    #print("oidList: ", oidList)
+    #print("fidList: ", fidList)
+
+    for i in range(oidListLength):
+        if tempOID == oidList[i]:
+            xListTemp.append(xList[i])
+            yListTemp.append(yList[i])
+            errorListTemp.append(errorList[i])
+            if fidList[i] == 1:
+                fmtToBeUsed = 'go'
+            else:
+                fmtToBeUsed = 'ro'
+
+    # plot for oid #1
+    plt.errorbar(xListTemp, yListTemp, yerr = errorListTemp, fmt=fmtToBeUsed, markersize=3) 
+
+    # use .format
+    plt.title("Star 2 Light Curve for Object {}".format(tempOID))
+    plt.xlabel("time")
+    plt.ylabel("brightness")
+    plt.show()
+
+    # here we need to find the index of the next unique oid
+
+    # this block is not completely general
+    # it assumes there is at least two unique oid's in the list (starData.txt's from IRSA)
+    newOID = oidList[index+1]
+    while newOID == tempOID:
+            index += 1
+            newOID = oidList[index+1]
+
+    # when here (out of above loop) newOID, is truly 'new'
+
+    firstIndexOfNextOid = index+1
+
+    tempOID = oidList[firstIndexOfNextOid]
+    for i in range(oidListLength):
+        if tempOID == oidList[i]:
+            xListTemp.append(xList[i])
+            yListTemp.append(yList[i])
+            errorListTemp.append(errorList[i])
+            if fidList[i] == 1:
+                fmtToBeUsed = 'go'
+            else:
+                fmtToBeUsed = 'ro'
+
+    # plot for oid #2
+    plt.errorbar(xListTemp, yListTemp, yerr = errorListTemp, fmt=fmtToBeUsed, markersize=3) 
+    plt.title("Star 2 Light Curve for Object {}".format(tempOID))
+    plt.xlabel("time")
+    plt.ylabel("brightness")
+    plt.show()
+
+    # need to find first index of next OID (the third oid in the list, if it exists)
+    # need to also make code for if 2nd oid does not exist (general version - but acutally
+    # for our purposes here, we don't need that)
+
+    newOID = oidList[index+1]
+    counter = 0
+    # listLength - (index+1) works!
+    while newOID == tempOID and counter < (listLength - (index+1)):
+        index += 1
+        newOID = oidList[index+1]
+        counter += 1
+
+    firstIndexOfNextOid = index+1
+
+    # because repeating code (i.e. copy & pasting this => could use a loop -
+    # could do so by figuring out before the loop how many unique OID's
+    # then looping that many times)
+
+    tempOID = oidList[firstIndexOfNextOid]
+    for i in range(oidListLength):
+        if tempOID == oidList[i]:
+            xListTemp.append(xList[i])
+            yListTemp.append(yList[i])
+            errorListTemp.append(errorList[i])
+            if fidList[i] == 1:
+                fmtToBeUsed = 'go'
+            else:
+                fmtToBeUsed = 'ro'
+
+    # plot for oid #3
+    plt.errorbar(xListTemp, yListTemp, yerr = errorListTemp, fmt=fmtToBeUsed, markersize=3) 
+    plt.title("Star 2 Light Curve for Object {}".format(tempOID))
+    plt.xlabel("time")
+    plt.ylabel("brightness")
+    plt.show()
+
+def plotLightCurveOld(filename):
     """ This function just works with time, mag, and error,
         i.e. does not filter by oid.
 
@@ -84,121 +251,7 @@ def plotLightCurve(filename):
     plt.xlabel("time")
     plt.ylabel("brightness")
     plt.show()
-  
-def plotLightCurvePrime(filename):
-    """ This function works with time, mag, error and DOES filter by oid.
-        Takes in four lists of data for each of the above mentioned things
-        and then makes three plots (of the three objects), which is what
-        is meant by filtering by oid
-
-        Arguments:
-            filename (string) : name of data txt file       
-    """
-
-    xList = []
-    yList = []
-    errorList = []
-    oidList = []
-    fidList = []
-
-    fin = open(filename)
-    for line in fin:
-        #line = line.strip()
-        line = line.split() # Splits each line into constituent numbers. 
-        xList.append(line[0])
-        yList.append(line[1])
-        errorList.append(line[2])
-        oidList.append(line[3])
-    fin.close()
-
-    xListPrime = []
-    for element in xList:
-        xListPrime.append(float(element))
-    xList = xListPrime # so not using 'xListPrime' going forward
-
-    yListPrime = []
-    for element in yList:
-        yListPrime.append(float(element))
-    yList = yListPrime
-
-    errorListPrime = []
-    for element in errorList:
-        errorListPrime.append(float(element))
-    errorList = errorListPrime
-
-    oidListPrime = []
-    for element in oidList:
-        oidListPrime.append(int(element))
-    oidList = oidListPrime
-
-    fidListPrime = []
-    for element in fidList:
-        fidListPrime.append(int(element))
-    fidList = fidListPrime
-
-    """
-    This plots all objects ID's on same plot:
-    plt.errorbar(xList, yList, yerr = errorList, fmt='ro', markersize=3) 
-    plt.title("Star 2 Light Curve")
-    plt.xlabel("time")
-    plt.ylabel("brightness")
-    plt.show()
-    """
-
-    xListTemp = []
-    yListTemp = []
-    errorListTemp = []
-
-    # first make the code for three oid's
-    # then generalize code so it works for any number
-    # (generalized code better but don't need to right now,
-    # not for what I am trying to do here)
-
-    tempOID = oidList[0]
-
-    oidListLength = len(oidList)
-
-    for i in range(oidListLength):
-        if tempOID == oidList[i]:
-            xListTemp.append(xList[i])
-            yListTemp.append(yList[i])
-            errorListTemp.append(errorList[i])
-
-    # plot for oid #1
-    plt.errorbar(xListTemp, yListTemp, yerr = errorListTemp, fmt='go', markersize=3) 
-    plt.title("Star 2 Light Curve for Object 226831060005494")
-    plt.xlabel("time")
-    plt.ylabel("brightness")
-    plt.show()
-
-    tempOID = oidList[1]
-    for i in range(oidListLength):
-        if tempOID == oidList[i]:
-            xListTemp.append(xList[i])
-            yListTemp.append(yList[i])
-            errorListTemp.append(errorList[i])
-
-    # plot for oid #2
-    plt.errorbar(xListTemp, yListTemp, yerr = errorListTemp, fmt='ro', markersize=3) 
-    plt.title("Star 2 Light Curve for Object 226832060006908")
-    plt.xlabel("time")
-    plt.ylabel("brightness")
-    plt.show()
-
-    tempOID = oidList[10]
-    for i in range(oidListLength):
-        if tempOID == oidList[i]:
-            xListTemp.append(xList[i])
-            yListTemp.append(yList[i])
-            errorListTemp.append(errorList[i])
-
-    # plot for oid #3
-    plt.errorbar(xListTemp, yListTemp, yerr = errorListTemp, fmt='ro', markersize=3) 
-    plt.title("Star 2 Light Curve for Object 26832000005734")
-    plt.xlabel("time")
-    plt.ylabel("brightness")
-    plt.show()
 
 if __name__ == '__main__':
     filename = input("Enter the data file name: ")
-    plotLightCurvePrime(filename)
+    plotLightCurve(filename)
