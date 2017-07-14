60160827c1ac097e3bcec0a6edbcbc1ffde6c327
Ethan Marx <ethanmarx@Ethans-MacBook-Pro.local>
2017-07-14 11:57:33 -0500

    Added Gaussian process modeling to Lightcurve class, Added dictionary to store Rchi2 values as they are calculated

--- LSST/SuperNovaLightCurves/Lightcurve_class.py
+++ LSST/SuperNovaLightCurves/Lightcurve_class.py
@@ -1,5 +1,5 @@
 #Sample Lightcurve class with Kapernka model 
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, minimize
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -7,12 +7,14 @@ from JSON_to_DF import JSON_to_DataFrame
 import ntpath
 import json
 import pandas as pd
+import celerite
 
 class Supernovae:
 	
 	def __init__(self, path):
 		self.name = os.path.splitext(os.path.basename(path))[0]
 		self.path = path
+		
 		
 
 	def meta_data(self):
@@ -42,6 +44,8 @@ class Supernovae:
 				Lightcurves[label] = filter_lightcurve(data.time.values, data.magnitude.values, data.e_magnitude.values, str(x[0]), keys, self.path)
 			self.Lightcurves = Lightcurves
 
+
+
 	def serialize(self):
 		pass
 
@@ -54,7 +58,7 @@ class filter_lightcurve(Supernovae):
 		#store time and band
 		super().__init__(path)
 		super().meta_data()
-
+		self.Rchi2 = {}
 		self.keys = keys
 		self.time = time
 		self.band = band
@@ -87,30 +91,70 @@ class filter_lightcurve(Supernovae):
 	
 
 	#Function to calculate RMSE, given a fit function
-	def calc_Rchi2(self, fit, degree=0):
+	def calc_Rchi2_poly(self, degree):
 		flux_predictions = np.empty(self.flux.shape) * np.nan
 		#loop to run 'leave one out' CV
 		for ind, f in enumerate(self.flux):
 			flux_del = np.delete(self.flux, ind)
-			times_del = np.delete(self.time, ind)
-			if(degree != 0):
-				Coeffs = np.polyfit(times_del, flux_del, degree)
-				f = np.poly1d(Coeffs)
-				ypred = f(self.time[ind])
-			else:
-				Coeffs, Covar = curve_fit(fit, times_del, flux_del, self.kap_prior[self.band], bounds=self.kap_param_bounds)
-				ypred = fit(self.time[ind], Coeffs[0], Coeffs[1], Coeffs[2], Coeffs[3], Coeffs[4], Coeffs[5])
+			time_del = np.delete(self.time, ind)
+			flux_err_del = np.delete(self.flux_err, ind)
+			Coeffs = np.polyfit(time_del, flux_del, degree, w=flux_err_del)
+			f = np.poly1d(Coeffs)
+			ypred = f(self.time[ind])
 			flux_predictions[ind] = ypred
-
-		
 		
 		#Root Mean Square Error calculations
 		dif = (flux_predictions - self.flux)/self.flux_err
 		temp = np.sum(dif**2)
 		temp = temp / (len(self.flux) - 6)
 		Rchi2 = np.sqrt(temp)
+		self.Rchi2['polynomial_' + str(degree)] = Rchi2
 		return Rchi2
 
+	def calc_Rchi2_func(self, fit, name):
+		flux_predictions = np.empty(self.flux.shape) * np.nan
+		#loop to run 'leave one out' CV
+		for ind, f in enumerate(self.flux):
+			flux_del = np.delete(self.flux, ind)
+			time_del = np.delete(self.time, ind)
+			flux_err_del = np.delete(self.flux_err, ind)
+			Coeffs, Covar = curve_fit(fit, time_del, flux_del, self.kap_prior[self.band], sigma=flux_err_del, bounds=self.kap_param_bounds)
+			ypred = fit(self.time[ind], Coeffs[0], Coeffs[1], Coeffs[2], Coeffs[3], Coeffs[4], Coeffs[5])
+			flux_predictions[ind] = ypred
+		
+		#Root Mean Square Error calculations
+		dif = (flux_predictions - self.flux)/self.flux_err
+		temp = np.sum(dif**2)
+		temp = temp / (len(self.flux) - 6)
+		Rchi2 = np.sqrt(temp)
+		self.Rchi2[name] = Rchi2
+		return Rchi2
+
+	def calc_Rchi2_GP(self, gp):
+		flux_predictions = np.empty(self.flux.shape) * np.nan
+		#loop to run 'leave one out' CV
+		for ind, f in enumerate(self.flux):
+			flux_del = np.delete(self.flux, ind)
+			time_del = np.delete(self.time, ind)
+			flux_err_del = np.delete(self.flux_err, ind)
+			gp.compute(time_del, flux_err_del)
+			initial_params = gp.get_parameter_vector()
+			bounds = gp.get_parameter_bounds()
+			r = minimize(self.neg_log_like, initial_params, method="L-BFGS-B", bounds=bounds, args=((flux_del, gp)))
+			gp.set_parameter_vector(r.x)
+			ypred, pred_var = gp.predict(flux_del, self.time[ind], return_var=True)
+			flux_predictions[ind] = ypred
+		
+		#Root Mean Square Error calculations
+		dif = (flux_predictions - self.flux)/self.flux_err
+		temp = np.sum(dif**2)
+		temp = temp / (len(self.flux) - 6)
+		Rchi2 = np.sqrt(temp)
+		self.Rchi2['GP'] = Rchi2
+		return Rchi2
+
+			
+			
 
 	#create polynomial fit function given degree
 	def polynomial_func(self, degree):
@@ -131,7 +175,7 @@ class filter_lightcurve(Supernovae):
 			plt.xlabel('time (days)')
 			plt.ylabel('relative flux')
 			plt.show()
-		return self.calc_Rchi2(f, degree=degree)
+		return self.calc_Rchi2_poly(degree)
 	
 	#initialize kapernka functin paramater bounds, and priors
 	kap_param_bounds = ([10*-5,10*-5,0,0,0,0], [1000,100,100,100,100,100])
@@ -159,6 +203,7 @@ class filter_lightcurve(Supernovae):
 
 	#Plot kapernka best fit and return Rchi2
 	def Kapernka_fit_plot(self, plot=True):
+		name = 'Kapernka'
 		if(plot):
 			fitCoeffs, Covars = curve_fit(self.Kapernka_func, self.time, self.flux, self.kap_prior[self.band], sigma=self.flux_err, bounds=self.kap_param_bounds)
 			bft = np.linspace(self.time[0], self.time[-1])
@@ -170,7 +215,7 @@ class filter_lightcurve(Supernovae):
 			plt.ylabel('relative flux')
 			plt.title(str(self.name) + str( self.band) + ' band Kapernka fit')
 			plt.show()
-		return self.calc_Rchi2(self.Kapernka_func, degree=0)
+		return self.calc_Rchi2_func(self.Kapernka_func, name)
 
 	#Bazin model
 	def Bazin_func(self, t, A, t_0, Trise, Tfall, a1, a2):
@@ -182,6 +227,7 @@ class filter_lightcurve(Supernovae):
 
 	#Plot Bazin best fit and return Rchi2
 	def Bazin_fit_plot(self, plot=True):
+		name = 'Bazin'
 		if(plot):
 			fitCoeffs, Covars = curve_fit(self.Bazin_func, self.time, self.flux, self.kap_prior[self.band], sigma=self.flux_err,  bounds=self.kap_param_bounds)
 			bft = np.linspace(self.time[0], self.time[-1])
@@ -190,7 +236,49 @@ class filter_lightcurve(Supernovae):
 			plt.errorbar(self.time, self.flux, yerr=self.flux_err, color='blue', label= self.band, fmt = 'o', markersize = 2.5)
 			plt.plot(bft, bestfit_flux, color = 'blue', label ='best fit')
 			plt.show()
-		return self.calc_Rchi2(self.Bazin_func, degree=0)
+		return self.calc_Rchi2_func(self.Bazin_func, name)
+
+	def neg_log_like(self, params, flux, gp):
+			gp.set_parameter_vector(params)
+			return -gp.log_likelihood(flux)
+
+	
+
+
+	def Gaussian_process(self, kernel, plot=True):
+		mean = np.mean(self.flux)
+		
+		gp = celerite.GP(kernel, mean=mean)
+		
+		gp.compute(self.time, self.flux_err)
+
+		initial_params = gp.get_parameter_vector()
+		bounds = gp.get_parameter_bounds()
+		r = minimize(self.neg_log_like, initial_params, method="L-BFGS-B", bounds=bounds, args=(self.flux, gp))
+		gp.set_parameter_vector(r.x)
+		pred_mean, pred_var = gp.predict(self.flux, self.time, return_var=True)
+
+		x = np.linspace(self.time[0], self.time[-1], 10000)
+		pred_std = np.sqrt(pred_var)
+		color = "#ff7f0e"
+		plt.figure()
+		plt.plot(self.time, pred_mean, label='GP model')
+		plt.errorbar(self.time, self.flux, fmt='o', yerr=self.flux_err, label= self.band + ' band', markersize= 2.5)
+		plt.fill_between(self.time, pred_mean+pred_std, pred_mean-pred_std, color=color, alpha=0.3,
+		                 edgecolor="none")
+		plt.legend()
+		plt.title('')
+		plt.xlabel('time (days)')
+		plt.ylabel('relative flux')
+
+		plt.title('SN2011fe ' + self.band + ' data')
+		plt.show()
+		return self.calc_Rchi2_GP(gp)
+		
+		
+
+
+
 
 
 
